4dae8a7bac5e6eeb436d204bb046dfeb76d70185
einarformica-code <einar.formica@iesnervion.es>
2026-01-08 14:40:03 +0100

    zxfwertger

--- Unidad3/src/boletin4/ejercicio4.java
+++ Unidad3/src/boletin4/ejercicio4.java
@@ -1,5 +1,50 @@
 package boletin4;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
-public interface ejercicio4 {
+public class ejercicio4 {
+	
+	//Definimos fx   nombre  (tipo nombre  tipo  nombre) tipos de nombres que se tienen que usar.
+ 	public static int buscar(int lista[], int clave) {
+		for (int i =0; i<lista.length; i++) {
+			
+			if (lista[i]==clave) {
+			return i;
+		}
+			
+		}	
+		return -1;//Si no encuentra la clave em la lista, devuelve enos uno.
+	}
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		int longitudLista; // Variable don de se almacenará la longitud de la lista.
+		int clave; //Variable que el usuario buscará en la lista.
+		Random random=new Random();
+		
+		System.out.println("Longitud del array");//Input longitud de la lista.
+		longitudLista = teclado.nextInt();
+		
+		System.out.println("Cual es la clave que desea buscar");
+		clave=teclado.nextInt();
+	
+		
+		int lista[]=new int[longitudLista]; //Creamos arrat de misma longitud.
+		
+		//Rellenamos tabla con numeros aleatorios.
+		
+		
+		for( int i = 0; i< lista.length; i++) {
+			lista[i]=random.nextInt(1,101);//La lista se rellenará de valores del 1 al 100.
+		}
+		int posicion = buscar(lista, clave);
+		System.out.println(Arrays.toString(lista));
+		System.out.println("Se ha encontrado en la posicion" + posicion);
+		
+		
+		
+		
+		
+	}
 
 }
---
+++ Unidad3/src/boletin4/ejercicio5.java
@@ -0,0 +1,15 @@
+package boletin4;
+
+public class ejercicio5 {
+	public static String revertir (String holamundo) {
+		new holamundo(original).reverse().toString();
+		return holamundo;
+		
+		
+	}
+	public static void main(String[] args) {
+		String holamundo=  "Hola mundo";
+		revertir(holamundo);
+	}
+
+}
